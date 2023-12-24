04047275476818ddeb976bba35770e61329be9d3
ninja <ninja@gmai.com>
2023-12-24 20:56:30 +0300

    first push 1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int x){return x+=5;}
+int fun2(int *x){return *x+=5;}
+
+int fun3(int &x){return x+=5;}
+
+
+int main()
+{
+
+int x=2;
+int y=2;
+int z=2;
+cout<<fun(x)<<"=>"<<x<<endl;
+cout<<fun2(&y)<<"=>"<<y<<endl;
+cout<<fun3(z)<<"=>"<<z<<endl;
+
+
+
+}
