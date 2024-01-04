6a99e0807c10e7c48b3eef8a770441199243f5b2
ninja <ninja@gmai.com>
2024-01-04 11:31:32 +0300

    database

---
+++ fruitkha-1.0.0/database/restaurant.sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04 يناير 2024 الساعة 09:27
+-- إصدار الخادم: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `img_web`
+--
+
+CREATE TABLE `img_web` (
+  `id_location` int(11) NOT NULL,
+  `location_img_web` varchar(150) NOT NULL,
+  `img` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `item`
+--
+
+CREATE TABLE `item` (
+  `id_item` int(11) NOT NULL,
+  `name_item` varchar(50) NOT NULL,
+  `descr_item` varchar(150) DEFAULT NULL,
+  `price_item` double NOT NULL,
+  `old_price_item` double DEFAULT NULL,
+  `id_menu` int(11) NOT NULL,
+  `img_item` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `name_menu` int(11) NOT NULL,
+  `descr_menu` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `note`
+--
+
+CREATE TABLE `note` (
+  `id_note` int(11) NOT NULL,
+  `note_text` text DEFAULT NULL,
+  `date_note` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(11) NOT NULL,
+  `date_order` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `total_order` double DEFAULT NULL,
+  `descr_order` text DEFAULT NULL,
+  `processing_order` tinyint(1) DEFAULT NULL,
+  `delivery_order` tinyint(1) DEFAULT NULL,
+  `address_ccostomer` varchar(200) DEFAULT NULL,
+  `id_pay` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_user_emp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `id_order` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `pay_type`
+--
+
+CREATE TABLE `pay_type` (
+  `id_pay` int(11) NOT NULL,
+  `name_pay` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `email_user` varchar(50) NOT NULL,
+  `pass_user` varchar(100) NOT NULL,
+  `phone_user` varchar(20) DEFAULT NULL,
+  `activ_order` int(11) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `street` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `img_user` varchar(250) DEFAULT NULL,
+  `name_user` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `img_web`
+--
+ALTER TABLE `img_web`
+  ADD PRIMARY KEY (`id_location`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id_item`),
+  ADD KEY `fk_menu` (`id_menu`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`id_note`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `fk_user` (`id_user`),
+  ADD KEY `fk_user_emp` (`id_user_emp`),
+  ADD KEY `fk_pay` (`id_pay`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD KEY `fk_order` (`id_order`),
+  ADD KEY `fk_item` (`id_item`);
+
+--
+-- Indexes for table `pay_type`
+--
+ALTER TABLE `pay_type`
+  ADD PRIMARY KEY (`id_pay`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `fk_activ_order` (`activ_order`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `img_web`
+--
+ALTER TABLE `img_web`
+  MODIFY `id_location` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `note`
+--
+ALTER TABLE `note`
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pay_type`
+--
+ALTER TABLE `pay_type`
+  MODIFY `id_pay` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- قيود الجداول المُلقاة.
+--
+
+--
+-- قيود الجداول `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`);
+
+--
+-- قيود الجداول `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_pay`) REFERENCES `pay_type` (`id_pay`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`id_user_emp`) REFERENCES `user` (`id_user`);
+
+--
+-- قيود الجداول `order_list`
+--
+ALTER TABLE `order_list`
+  ADD CONSTRAINT `order_list_ibfk_1` FOREIGN KEY (`id_item`) REFERENCES `item` (`id_item`),
+  ADD CONSTRAINT `order_list_ibfk_2` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
